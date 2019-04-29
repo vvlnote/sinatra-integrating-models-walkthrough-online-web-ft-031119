723ffe160301d9f7af006c8a71536c857f8cbad2
Joy Lin <vvlnote@hotmail.com>
2019-04-29 05:06:12 +0000

    Done.

--- spec/integrating_models_sinatra_walkthrough_spec.rb
+++ spec/integrating_models_sinatra_walkthrough_spec.rb
@@ -17,7 +17,8 @@ describe App do
     it "displays string results" do
       visit '/'
 
-      fill_in(:user_text, :with => "Green Eggs and Ham")
+      fill_in(:user_text, :with => "
+      ")
       click_button "submit"
       expect(page.status_code).to eq(200)
       expect(page).to have_text("Number of Words: 4")
